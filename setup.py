214f8c878e381515c1b11d2c4b396b8bb7d6d94a
vforgione <v.forgione@gmail.com>
2015-08-19 11:25:49 -0500

    reqs

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from distutils.core import setup
 
 setup(
     name="django-influxer-client",
-    version="2.0.0",
+    version="2.0.1",
     description="Modules and JS to work with Django-Influxer integration",
     author="Vince Forgione",
     author_email="vforgione@theonion.com",
     url="https://github.com/theonion/django-influxer-client",
     packages=["djinfluxer"],
     install_requires=[
-        "Django>1.7,<1.8",
-        "influxdb==0.1.13",
+        "influxdb",
     ]
 )
